0762a643c2a72aa4f9d9f556315bc13a15293f7c
kaushal kumar <kaushal.k.0014@gmail.com>
2025-03-29 21:46:25 +0530

    adding new repo service registry

---
+++ Service-Registry-API/src/main/java/com/service/registry/Service_Registry_API/ServiceRegistryApiApplication.java
@@ -0,0 +1,13 @@
+package com.service.registry.Service_Registry_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegistryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApiApplication.class, args);
+	}
+
+}
