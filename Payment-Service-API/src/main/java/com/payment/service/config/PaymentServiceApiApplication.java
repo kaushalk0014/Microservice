0e945045b3d6fcce55d5e160edb6db4ef013600a
kaushal kumar <kaushal.k.0014@gmail.com>
2025-03-30 16:41:18 +0530

    added eureka discovery client

--- Payment-Service-API/src/main/java/com/payment/service/config/PaymentServiceApiApplication.java
+++ Payment-Service-API/src/main/java/com/payment/service/config/PaymentServiceApiApplication.java
@@ -3,6 +3,7 @@ package com.payment.service.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -11,6 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages = {"com.payment.service.entiry"})
 @EnableJpaRepositories(basePackages = {"com.payment.service.repositry"})
 @ComponentScan(basePackages = {"com.payment.service"})
+@EnableDiscoveryClient
 public class PaymentServiceApiApplication {
 
 	public static void main(String[] args) {
