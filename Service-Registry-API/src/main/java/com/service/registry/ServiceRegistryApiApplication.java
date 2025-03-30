1c80992fa9b9ef0ebe68ca0cf7bde1076c1fb910
kaushal kumar <kaushal.k.0014@gmail.com>
2025-03-30 16:42:24 +0530

    added eureka server configuration

--- Service-Registry-API/src/main/java/com/service/registry/ServiceRegistryApiApplication.java
+++ Service-Registry-API/src/main/java/com/service/registry/ServiceRegistryApiApplication.java
@@ -1,9 +1,11 @@
-package com.service.registry.Service_Registry_API;
+package com.service.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceRegistryApiApplication {
 
 	public static void main(String[] args) {
