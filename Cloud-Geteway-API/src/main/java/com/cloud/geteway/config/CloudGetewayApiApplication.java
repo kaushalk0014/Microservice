56a3690c344c4f850c4d8cddc6a74c131c0bbb76
kaushal kumar <kaushal.k.0014@gmail.com>
2025-04-01 21:48:10 +0530

    fixed API geteway issue

--- Cloud-Geteway-API/src/main/java/com/cloud/geteway/config/CloudGetewayApiApplication.java
+++ Cloud-Geteway-API/src/main/java/com/cloud/geteway/config/CloudGetewayApiApplication.java
@@ -1,8 +1,11 @@
 package com.cloud.geteway.config;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,5 +14,13 @@ public class CloudGetewayApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CloudGetewayApiApplication.class, args);
 	}
+	
+	@Bean
+    public RouteLocator configureRoute(RouteLocatorBuilder builder) {
+       return builder.routes()
+      .route("paymentId", r->r.path("/payment/**").uri("http://localhost:9292")) //static routing
+      .route("orderId", r->r.path("/order/**").uri("lb://ORDER-SERVICE")) //dynamic routing
+      .build();
+    }
 
 }
